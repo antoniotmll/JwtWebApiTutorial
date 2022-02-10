bd3bce9b6222a201818226e381cd9d552e658cce
Antonio <antoniotmll@gmail.com>
2022-02-10 21:31:26 +0100

    Added auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            //TODO: method
+
+            return user;
+        }
+
+
+    }
+}
