f7c3928933c01d1fbbbd6013d9ebff306e2bcb69
Antonio <antoniotmll@gmail.com>
2022-02-12 18:39:15 +0100

    Added createtoken method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace JwtWebApiTutorial.Controllers
@@ -9,6 +12,12 @@ namespace JwtWebApiTutorial.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        public readonly IConfiguration _configuration;
+        
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserDto request)
@@ -35,7 +44,30 @@ namespace JwtWebApiTutorial.Controllers
                 return BadRequest("Wrong password");
             }
 
-            return Ok("MY CRAZY TOKEN");
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+        
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jtw = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jtw;
         }
         
 
